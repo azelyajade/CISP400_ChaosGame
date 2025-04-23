7248256bfd4d5237563de5ec36e8a237392c96fa
pollyology <pcagaanan01@gmail.com>
2025-04-23 09:27:28 -0700

    Implement ChaosGame.cpp w/ new features
    Added: menu to choose color
    Added: restart game functionality
    Updated: point drawing functionality

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -13,8 +13,19 @@
 using namespace sf;
 using namespace std;
 
+std::string colorToString(Color& color)
+{
+	if (color == Color::Red) return "Red";
+	if (color == Color::Green) return "Green";
+	if (color == Color::Blue) return "Blue";
+	if (color == Color::Yellow) return "Yellow";
+}
 int main()
 {
+// +=============================================+
+// |		INITIALIZE VARIABLES		 |
+// +=============================================+
+// 
 	// Create a video mode object
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
@@ -33,11 +44,62 @@ int main()
 
 	Text text; // set text settings
 	text.setFont(font);
-	text.setString("Pick any 3 points. Then pick a 4th point to generate Sierpinski Triangle!");
+	text.setString("Welcome to the Chaos Game!");
 	text.setCharacterSize(50);
-	text.setFillColor(Color::White);
-	text.setPosition(20, 20);
+	text.setFillColor(Color::Green);
+	text.setPosition(14, 14);
 
+	Text prompt;
+	prompt.setFont(font);
+	prompt.setString("Press 'SPACE' to continue...");
+	prompt.setCharacterSize(25);
+	prompt.setFillColor(Color::Green);
+
+	FloatRect promptBounds = prompt.getLocalBounds();
+	prompt.setOrigin(0, promptBounds.height);
+	prompt.setPosition(10, window.getSize().y - 10);
+
+	// -----------------------------------------
+	//		Menu Handling
+	// -----------------------------------------
+		Text menu_1;
+		menu_1.setFont(font);
+		menu_1.setString("1. Red");
+		menu_1.setCharacterSize(30);
+		menu_1.setFillColor(Color::Red);
+		menu_1.setPosition(14, 100);
+
+		Text menu_2;
+		menu_2.setFont(font);
+		menu_2.setString("2. Green");
+		menu_2.setCharacterSize(30);
+		menu_2.setFillColor(Color::Green);
+		menu_2.setPosition(14, 150);
+
+		Text menu_3;
+		menu_3.setFont(font);
+		menu_3.setString("3. Blue");
+		menu_3.setCharacterSize(30);
+		menu_3.setFillColor(Color::Blue);
+		menu_3.setPosition(14, 200);
+
+		Text menu_4;
+		menu_4.setFont(font);
+		menu_4.setString("4. Yellow");
+		menu_4.setCharacterSize(30);
+		menu_4.setFillColor(Color::Yellow);
+		menu_4.setPosition(14, 250);
+	// -----------------------------------------
+
+	const int pointsPerFrame = 1; // Constant for num of points we wanna draw each frame
+	bool showMenu = false;
+	Color colorChoice;
+
+
+// +=====================================+
+// |		GAME LOOP		 |
+// +=====================================+
+//
 	while (window.isOpen())
 	{
 		/*
@@ -53,31 +115,76 @@ int main()
 				// Quit the game when the window is closed
 				window.close();
 			}
-			if (event.type == sf::Event::MouseButtonPressed)
-			{
-				if (event.mouseButton.button == sf::Mouse::Left)
-				{
-					std::cout << "the left button was pressed" << std::endl;
-					std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-					std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
-					if (vertices.size() < 3)
+			if (Keyboard::isKeyPressed(Keyboard::Space))
+			{
+				std::cout << "spacebar was pressed" << std::endl;
+				text.setString("Choose a color: ");
+				prompt.setString("");
+				showMenu = true;
+			}
+
+			if (showMenu && event.type == Event::KeyPressed) // Menu logic
+			{
+				if (event.key.code == Keyboard::Num1) { colorChoice = Color::Red; } // Choice red
+				else if (event.key.code == Keyboard::Num2) { colorChoice = Color::Green; } // Choice green
+				else if (event.key.code == Keyboard::Num3) { colorChoice = Color::Blue; } // Choice blue
+				else if (event.key.code == Keyboard::Num4) { colorChoice = Color::Yellow; } // Choice yellow
+
+				if (event.key.code >= Keyboard::Num1 && event.key.code <= Keyboard::Num4)
+				{
+					showMenu = false;
+
+					prompt.setFillColor(colorChoice);
+					prompt.setString(colorToString(colorChoice) + " was selected");
+					text.setFillColor(colorChoice);
+					text.setString("Click 3 points to make a triangle.");
+				}
+			}
+
+			if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+			{
+				std::cout << "the left button was pressed" << std::endl;
+				std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+				std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+
+				if (vertices.size() < 3)
+				{
+					vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					prompt.setString("");
+					if (vertices.size() == 3)
 					{
-						vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+						text.setString("Now, click anywhere to make a starting point!");
 					}
-					else if (points.size() == 0)
+				}
+				else if (points.size() == 0)
+				{
+					if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) ///fourth click 
 					{
-						if (event.type == sf::Event::MouseButtonPressed) ///fourth click 
-						{
-							if (event.mouseButton.button == sf::Mouse::Left)
-							{
-								points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));	///push back to points vector
-							}
-						}
+						text.setString("WOAAAAAAAHHHHHHHHHHHHH!!!!");
+						points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));	///push back to points vector
 					}
 				}
 			}
+
+			if (points.size() == 1000)
+			{
+				prompt.setFillColor(Color::Green);
+				prompt.setString("Nice! Press 'R' to restart...");
+
+				if (Keyboard::isKeyPressed(Keyboard::R))
+				{
+					points.clear();
+					vertices.clear();
+					window.clear();
+					text.setString("Press 'SPACE' to choose a color");
+					prompt.setString("");
+					showMenu = false;
+					text.setFillColor(Color::Green);
+				}
+			}
 		}
+
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
@@ -88,9 +195,9 @@ int main()
 		****************************************
 		*/
 
-		if (points.size() > 0)
+		if (points.size() > 0 && points.size() < 1000) // Hard limit of 1000 points
 		{
-			while (points.size() < 1000)
+			for (int i = 0; i < pointsPerFrame; i++) // Each loop, we only store a few points per frame of runtime (pointsPerFrame)
 			{
 				// initialize last point as a temp value
 				Vector2f temp = points.back();
@@ -99,7 +206,7 @@ int main()
 				int randNum = rand() % 3;
 				Vector2f randVert(vertices[randNum].x, vertices[randNum].y);
 				
-				// cal midpoint	
+				// call midpoint	
 				float midX = (temp.x + randVert.x) / 2.0;
 				float midY = (temp.y + randVert.y) / 2.0;
 				Vector2f midpoint(midX, midY);
@@ -124,17 +231,30 @@ int main()
 			window.draw(rect);
 		}
 		///TODO:  Draw points
+
+
 		for (unsigned long int i = 0; i < points.size(); i++)
 		{
 			CircleShape circle(1);
 			circle.setRadius(1);
 			circle.setPosition(Vector2f(points[i].x, points[i].y));
-			circle.setFillColor(Color::Green);
+			circle.setFillColor(colorChoice);
 			window.draw(circle);
+			
 		}
 
-		// Draw text
+
 		window.draw(text);
+		window.draw(prompt);
+
+		if (showMenu)
+		{
+			window.draw(menu_1);
+			window.draw(menu_2);
+			window.draw(menu_3);
+			window.draw(menu_4);
+		}
+
 		window.display();
 	}
 }
